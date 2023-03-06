91b2fda57853a03f763fd290492d5dcb96416530
tuxianchao <xianchaot@gmail.com>
2023-03-07 02:43:06 +0800

    root_dir inject

---
+++ src/test_root_dir/root_dir_test.cpp
@@ -0,0 +1,10 @@
+#include "root_directory.h"
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "root_dir is: " << root_dir << std::endl;
+    std::cout << "root_resource_dir is: " << root_resource_dir << std::endl;
+    return 0;
+}
