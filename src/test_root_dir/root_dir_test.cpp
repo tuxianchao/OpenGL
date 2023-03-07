99d67e64b2a8caa19424dfc701980765096060a7
tuxianchao <xianchaot@gmail.com>
2023-03-07 23:24:27 +0800

    vs folder

--- src/test_root_dir/root_dir_test.cpp
+++ src/test_root_dir/root_dir_test.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 
-    std::cout << "root_dir is: " << root_dir << std::endl;
-    std::cout << "root_resource_dir is: " << root_resource_dir << std::endl;
-    return 0;
+	std::cout << "root_dir is: " << root_dir << std::endl;
+	std::cout << "root_resource_dir is: " << root_resource_dir << std::endl;
+	return 0;
 }
