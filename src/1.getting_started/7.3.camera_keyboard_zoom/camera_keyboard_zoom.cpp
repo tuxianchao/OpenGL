a65f2111e69de7874258184724d91985facaf117
tuxianchao <xianchaot@gmail.com>
2023-03-14 01:33:30 +0800

    sync

--- src/1.getting_started/7.3.camera_keyboard_zoom/camera_keyboard_zoom.cpp
+++ src/1.getting_started/7.3.camera_keyboard_zoom/camera_keyboard_zoom.cpp
@@ -34,8 +34,8 @@ float lastTime          = 0.0f; // 上一帧的时间
 float yaw               = -90.0f; // yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
 float pitch             = 0.0f;
 bool firstMouse         = true;
-float lastX             = 800.0f / 2.0;
-float lastY             = 600.0 / 2.0;
+float lastX             = SCREEN_WIDTH / 2.0;
+float lastY             = SCREEN_HEIGHT / 2.0;
 float fov               = 45.0f;
 
 int main()
