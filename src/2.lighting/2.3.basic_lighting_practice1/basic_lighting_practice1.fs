b45ecad5b233c884f50a6df97c0abfb5605c0581
tuxianchao <xianchaot@gmail.com>
2023-05-02 00:35:32 +0800

    练习1  灯光盒子绕轴旋转

---
+++ src/2.lighting/2.3.basic_lighting_practice1/basic_lighting_practice1.fs
@@ -0,0 +1,40 @@
+#version 330 core
+out vec4 FragColor;
+  
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+in vec3 v_Normal;
+in vec3 v_FragPos;
+
+void main()
+{
+
+
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+
+    // 计算光源和片段位置之间的方向向量
+    vec3 norm = normalize(v_Normal);
+    vec3 lightDir = normalize(lightPos - v_FragPos);
+
+    // 对norm和lightDir向量进行点乘，计算光源对当前片段实际的漫反射影响。
+    // 结果值再乘以光的颜色，得到漫反射分量。两个向量之间的角度越大，漫反射分量就会越小
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+    
+
+    // 高光计算
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - v_FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    FragColor = vec4(result, 1.0);
+}
