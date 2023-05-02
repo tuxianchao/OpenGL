f361ead1c23a3ef6635ce6729133374db815a0f7
tuxianchao <xianchaot@gmail.com>
2023-05-02 18:12:11 +0800

    深度测试视图

---
+++ src/4.advanced_opengl/1.2.depth_testing_view/depth_testing_view.fs
@@ -0,0 +1,25 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 v_TexCoords;
+
+uniform sampler2D texture1;
+
+float near = 0.1; 
+float far  = 100.0; 
+
+float LinearizeDepth(float depth) 
+{
+    float z = depth * 2.0 - 1.0; // back to NDC 
+    return (2.0 * near * far) / (far + near - z * (far - near));    
+}
+
+void main()
+{
+    // FragColor = texture(texture1, v_TexCoords);
+    // FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
+
+
+    float depth = LinearizeDepth(gl_FragCoord.z) / far; // 为了演示除以 far
+    FragColor = vec4(vec3(depth), 1.0);
+}
